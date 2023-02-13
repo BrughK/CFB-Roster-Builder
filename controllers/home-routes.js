979dd87b6fcd559361d77b4202ace508d6343e60
jakester101 <55941541+jakester101@users.noreply.github.com>
2023-02-13 12:14:41 -0500

    added login/logout functionality

---
+++ controllers/home-routes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router
+
+
+
+/*-------------------------------------------------------
+example from module:
+
+// GET one painting
+router.get('/painting/:id', async (req, res) => {
+  try {
+    const dbPaintingData = await Painting.findByPk(req.params.id);
+
+    const painting = dbPaintingData.get({ plain: true });
+    res.render('painting', { painting, loggedIn: req.session.loggedIn });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+--------------------------------------------------------*/
+
+
+
+
+// Login route
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
+  });
+  
+  module.exports = router;
