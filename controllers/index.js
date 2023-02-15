19c4a9575680aa1f67c55f59ecba215c68527a51
Kasen's Desktop <kasenb1211@gmail.com>
2023-02-14 19:54:27 -0500

    Cleaned Up, and player card styling

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,4 @@
 const router = require("express").Router();
-// const Roster = require("../models/Roster");
-
 const apiRoutes = require("./api");
 const homeRoutes = require("./home-routes.js");
 const Players = require("../models/players");
@@ -12,11 +10,6 @@ router.get("/", (req, res) => {
   res.render("homepage");
 });
 
-// router.get("/roster", async (req, res) => {
-//   const players = await Players.findAll();
-//   res.render("roster", { players });
-// });
-
 router.get("/roster", async (req, res) => {
   const playerData = await Players.findAll().catch((err) => {
     res.json(err);
@@ -24,13 +17,5 @@ router.get("/roster", async (req, res) => {
   const players = playerData.map((player) => player.get({ plain: true }));
   res.render("roster", { players });
 });
-
-// router.get("/roster", async (req, res) => {
-//   res.render("roster");
-// });
-
-// router.get("/roster", (req, res) => {
-//   res.render("roster");
-// });
 
 module.exports = router;
